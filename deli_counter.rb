cf08cf7fed999a69b928aa5975544f7711429c6e
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-05 22:02:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,6 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-    katz_deli.each_with_index
+    katz_deli.each_with_index do ||
   end
 end
