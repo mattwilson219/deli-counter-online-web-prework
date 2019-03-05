8691cd8629eee43a036a27266719007b187c0b41
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-05 22:13:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,24 +7,9 @@ def line(katz_deli)
   else
     message = "The line is currently:"
     katz_deli.each_with_index do |value, index|
-    message += "#{index.to_i+1}. #{value}"  
+    message += " #{index.to_i+1}. #{value}"  
   end
     puts "#{message}"
 end
 end
 
-def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-    message="The line is currently:"
-
-    katz_deli.each_with_index do |value, index|
-      message += " #{index.to_i+1}. #{value}"
-    end
-
-    puts "#{message}"
-  end
-
-
-end
